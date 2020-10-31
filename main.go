b074490a7eee3b4143508eb2e125441db7f2d7ba
Gitea <gitea@fake.local>
2020-10-31 17:27:53 +0800

    update description

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func init() {
 func main() {
 	app := cli.NewApp()
 	app.Name = "Gitea"
-	app.Usage = "A painless self-hosted Git service"
+	app.Usage = "A painless self-hosted Git service -- patched by sysdl132"
 	app.Description = `By default, gitea will start serving using the webserver with no
 arguments - which can alternatively be run by running the subcommand web.`
 	app.Version = Version + formatBuiltWith()
